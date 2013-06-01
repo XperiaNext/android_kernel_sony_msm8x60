2d10249e4cb912e3c45e9f7606c12a1697bc07ac
paul-xxx <thepaulxxx@gmail.com>
2013-06-02 05:49:14 +0600

    ubuntu touch proc fs patch

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -337,6 +337,14 @@ static void task_cpus_allowed(struct seq_file *m, struct task_struct *task)
 	seq_putc(m, '\n');
 }
 
+#ifdef PHABLET_PROC_FS
+static void task_vpid(struct seq_file *m, struct task_struct *task)
+{
+	struct pid_namespace *ns = task_active_pid_ns(task);
+	seq_printf(m, "Vpid:\t%d\n", ns ? task_pid_nr_ns(task, ns) : 0);
+}
+#endif
+
 int proc_pid_status(struct seq_file *m, struct pid_namespace *ns,
 			struct pid *pid, struct task_struct *task)
 {
@@ -354,6 +362,9 @@ int proc_pid_status(struct seq_file *m, struct pid_namespace *ns,
 	task_cpus_allowed(m, task);
 	cpuset_task_status_allowed(m, task);
 	task_context_switch_counts(m, task);
+#ifdef PHABLET_PROC_FS
+	task_vpid(m, task);
+#endif
 	return 0;
 }
 
